256a00a47f6c6211c4fe39fb79551ca755a541e6
Karan Gupta <d22124440@mytudublin.ie>
2023-12-28 21:39:39 +0000

    Add footer component with logo and copyright information

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -1,5 +1,23 @@
+import Image from "next/image";
+import Link from "next/link";
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="border-t">
+      <div className="flex-center wrapper flex-between flex flex-col gap-4 p-5 text-center sm:flex-row">
+        <Link href="/">
+          <Image
+            src="/assets/images/logo.svg"
+            alt="Event Snap Logo"
+            width={128}
+            height={38}
+          />
+        </Link>
+
+        <p>2024 Event Snap. All Rights Reserved. </p>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
