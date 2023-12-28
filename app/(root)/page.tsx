f52c9c09ff0b2a3c2920fcb9a23ca707ebd44deb
Karan Gupta <d22124440@mytudublin.ie>
2023-12-28 16:23:45 +0000

    Add dependencies and update layout and tailwind configuration

---
+++ app/(root)/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <main>
+      <Button variant="destructive">Click Me</Button>
+    </main>
+  );
+}
